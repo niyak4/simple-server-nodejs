dce3983e4f6a1b391110e41e6efd9d9d6c3fc806
Yaroslav <keyla.prezent@gmail.com>
2020-07-03 18:17:09 +0300

    Added form

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { parse } = require('querystring');
 
 const server = http.createServer( (req, res) => {
-
-  //let filePath = path.join(__dirname, 'serverFiles', req.url);
+  if (req.method === 'POST') {
+    collectRequestData(req, parsedData => {
+      console.log(parsedData);
+      addToDB("DB.txt", parsedData);
+    });
+  }
   let filePath = path.join(__dirname, 'serverFiles', req.url === '/' ? 'index.html' : req.url);
   const ext = path.extname(filePath);
   let contentType = '';
@@ -44,10 +49,38 @@ const server = http.createServer( (req, res) => {
       res.end(content);
     }
   })
-})
+});
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server has been started on port: ${PORT}!`)
 });
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
+
+function addToDB(nameDB, data) {
+  if (fs.existsSync(nameDB)) {
+    fs.appendFile(nameDB, `id: ${data.id}\tname: ${data.name}\n`, 'ascii', function(error){
+      if(error) throw error;
+    });
+  } else {
+    fs.writeFile(nameDB, `id: ${data.id}\tname: ${data.name}\n`, 'ascii', function(error){
+      if(error) throw error;
+    });
+  }
+}
